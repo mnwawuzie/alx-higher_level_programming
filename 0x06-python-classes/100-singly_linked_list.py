cd011bfa9b51ee3e9c43fd1a9f6ddc38bf70d75b
PrimoCrypt <104681010+PrimoCrypt@users.noreply.github.com>
2024-04-27 02:43:09 +0100

    ADDED the files for the optional tasks

---
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -0,0 +1,18 @@
+            new.next_node = self.__head
+            self.__head = new
+        else:
+            tmp = self.__head
+            while (tmp.next_node is not None and
+                    tmp.next_node.data < value):
+                tmp = tmp.next_node
+            new.next_node = tmp.next_node
+            tmp.next_node = new
+
+    def __str__(self):
+        """Define the print() representation of a SinglyLinkedList."""
+        values = []
+        tmp = self.__head
+        while tmp is not None:
+            values.append(str(tmp.data))
+            tmp = tmp.next_node
+        return ('\n'.join(values))
