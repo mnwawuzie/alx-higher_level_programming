290e3760bf0739ad781732a521ceafd8b8a5c04b
PrimoCrypt <104681010+PrimoCrypt@users.noreply.github.com>
2024-05-10 14:54:22 +0100

    Updated the code for task 3

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,17 +1,17 @@
 #!/usr/bin/python3
-"""A script that:
-- takes in a URL,
-- sends a request to the URL
-- displays the body of the response (decoded in utf-8).
+"""Request and print response body, including error codes
 """
 
+import urllib.request
+from sys import argv
 
-if __name__ == "__main__":
-    import sys
-    from urllib import request, error
+
+if __name__ == '__main__':
+    url = argv[1]
 
     try:
-        with request.urlopen(sys.argv[1]) as res:
-            print(res.read().decode('UTF-8'))
-    except error.HTTPError as er:
-        print('Error code:', er.code)
+        with urllib.request.urlopen(url) as response:
+            print(response.read().decode('utf-8'))
+
+    except urllib.error.HTTPError as error:
+        print('Error code: {}'.format(error.code))
